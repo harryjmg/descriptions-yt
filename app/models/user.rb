7d275b081c1added448e9cd4a0ab7d5ead85392b
Harry Gueguen <harry@MacBook-Pro-de-Harry.local>
2020-09-23 15:12:56 +0200

    ✨ Can now log with Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,23 @@ class User < ApplicationRecord
   # :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable, :omniauthable
+         :confirmable, :trackable, :omniauthable,
+         omniauth_providers: [:google_oauth2]
 
+  serialize :name
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # users to be created if they don't exist
+    unless user
+        user = User.create(name: { first: data['first_name'], last: data['last_name'], full: data['name'] } ,
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.omniauth :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret], skip_jwt: true
+
 end
