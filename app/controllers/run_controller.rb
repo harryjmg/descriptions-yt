6f9e21ef8e367f15de693d294ee81ea47aa73582
Harry Gueguen <harry@MacBook-Pro-de-Harry.local>
2020-09-24 18:17:51 +0200

    ✨ UI Git diff like on Review changes

--- app/controllers/run_controller.rb
+++ app/controllers/run_controller.rb
@@ -17,39 +17,48 @@ class RunController < ApplicationController
     @videos = [
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri more word"
       },
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines"
       },
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri\n",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri\nBonjour"
       },
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
       },
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
       },
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
       },
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
       },
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
       },
       {
         title: "My last great video title is here and is a little long",
-        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
+        edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
       }
     ]
   end
