726581b074fa3381ced72a3321e739b8adde9ce4
Harry Gueguen <harry@MacBook-Pro-de-Harry.local>
2020-09-24 16:56:08 +0200

    ✨ Select videos Style is done

--- app/controllers/run_controller.rb
+++ app/controllers/run_controller.rb
@@ -1,5 +1,43 @@
 class RunController < ApplicationController
   def select_videos
+    @videos = [
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      },
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      },
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      },
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      },
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      },
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      },
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      },
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      },
+      {
+        title: "My last great video title is here and is a little long",
+        description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      }
+    ]
   end
 
   def edit_descriptions
