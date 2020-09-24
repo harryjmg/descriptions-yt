b2220534f6c0c7541d5f522abecd34a674c743c3
Harry Gueguen <harry@MacBook-Pro-de-Harry.local>
2020-09-24 11:20:50 +0200

    ✨ Run controller created

---
+++ app/controllers/run_controller.rb
@@ -0,0 +1,10 @@
+class RunController < ApplicationController
+  def select_videos
+  end
+
+  def edit_descriptions
+  end
+
+  def publish_changes
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'run/select_videos'
+  get 'run/edit_descriptions'
+  get 'run/publish_changes'
+  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   get 'home/index'
