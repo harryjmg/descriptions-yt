c3fd5c73c8aa4c7ebf8ebc45a1905a432ee1b798
Harry Gueguen <harry@MacBook-Pro-de-Harry.local>
2020-09-24 18:33:15 +0200

    ✨ Do it with blocks is better

--- app/controllers/run_controller.rb
+++ app/controllers/run_controller.rb
@@ -9,6 +9,7 @@ class RunController < ApplicationController
 
   def publish_changes
     set_videos
+    set_blocks
   end
 
   private
@@ -59,6 +60,19 @@ class RunController < ApplicationController
         title: "My last great video title is here and is a little long",
         description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri",
         edited_description: "The description of my last video is pretty long so i can see what it looks like on two lines my nigga cheri"
+      }
+    ]
+  end
+
+  def set_blocks
+    @blocks = [
+      {
+        content: "Je m'appelle Harry et j'adore le riz\nJ'aime aussi le fric\nJ'aime aussi les meufs\nLe dites pas à ma copine",
+        edited_content: "Je m'appelle Harry et j'adore le riz\nJ'aime aussi le fric\nJ'aime aussi les nems"
+      },
+      {
+        content: "Contactez moi\nSur facebook : lien fb\nSur mail : lien mail\n",
+        edited_content: "Contactez moi\nSur facebook : lien fb\nSur mail : lien mail\nA new line"
       }
     ]
   end
