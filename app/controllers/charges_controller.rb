e3dcb785238af3544db696bae12b5eabebccb8d8
Harry Gueguen <harry@MacBook-Pro-de-Harry.local>
2020-09-25 16:05:30 +0200

    ✨ Stripe implemented

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,9 +2,13 @@ class ChargesController < ApplicationController
   def new
   end
 
+  def index
+    redirect_to credit_index_path
+  end
+
   def create
     # Amount in cents
-    @amount = 500
+    @amount = 990
 
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
@@ -18,8 +22,12 @@ class ChargesController < ApplicationController
       currency: 'usd',
     })
 
+    if charge["paid"]
+      current_user.credit_add(300)
+    end
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_charge_path
+    redirect_to credit_index_path
   end
 end
